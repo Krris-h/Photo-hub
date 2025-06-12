23486bfc94576ad7dafe0a7f34b339dacdb779cf
Krris-h <krrishk148@gmail.com>
2025-06-12 22:19:14 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+// script.js
+function showWelcome() {
+  document.getElementById("welcome-message").style.display = "flex";
+}
+
+function closeWelcome() {
+  document.getElementById("welcome-message").style.display = "none";
+}
